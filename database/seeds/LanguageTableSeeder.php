87d8fa9d8a04f41cefa05fe13e1548e19c9aea14
Correia Dylan <dylan.correia@hotmail.com>
2018-06-12 15:25:22 +0200

    Language Seeder

---
+++ database/seeds/LanguageTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LanguageTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+	DB::table('language')->insert([
+	[
+		'id' => 1,
+		'languageName' => 'VF'
+	],
+	[
+		'id' => 2,
+		'languageName' => 'VOSTFR'
+	]
+	]);
+    }
+}
